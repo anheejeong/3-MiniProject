2e0a4db4612e43baa89a7209d5fd79dedbbc1172
anheejeong <eyrt6973@naver.com>
2023-08-08 02:14:40 +0900

    login file

--- 3rd-smallProject-login/client/src/App.js
+++ 3rd-smallProject-login/client/src/App.js
@@ -1,24 +1,67 @@
-import logo from './logo.svg';
-import './App.css';
+import { Router } from "express";
+import React from "react";
+import {
+  BrowserRouter,
+  Switch,
+  Route,
+  Link,
+  Routes
+} from "react-router-dom";
+
+function Home() {
+  return (
+    <div>
+      <h2>Home</h2>
+    </div>
+  )
+}
+
+function About() {
+  return (
+    <div>
+      <h2>About</h2>
+    </div>
+  )
+}
+
+function Dashboard() {
+  return (
+    <div>
+      <h2>Dashboard</h2>
+    </div>
+  )
+}
 
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <div>
+        <ul>
+          <li>
+            <link to='/'>Home</link>
+          </li>
+          <li>
+            <link to='/about'>About</link>
+          </li>
+          <li>
+            <link to='/dashboard'>Dashboard</link>
+          </li>
+        </ul>
+      </div>
+
+      <Routes>
+        <Route exact path="/" element={<Home />}></Route>
+        <Route path="/about" element={<About />}></Route>
+        <Route path="/dashboard" element={<Dashboard />}></Route>
+      </Routes>
+
+      {/* <Switch>
+        <Route exact path="/" component={Home} />    (4)
+        <Route path="/about" component={Photo} />
+        <Route path="/dashboard" component={Rooms} />
+      </Switch> */}
+    </BrowserRouter>
   );
 }
 
