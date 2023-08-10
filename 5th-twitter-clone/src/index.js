08bf3f228de122637b6f196d3685b03ec5067454
anheejeong <eyrt6973@naver.com>
2023-08-11 00:21:47 +0900

    setup firebase

--- 5th-twitter-clone/src/index.js
+++ 5th-twitter-clone/src/index.js
@@ -2,6 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 // import reportWebVitals from './reportWebVitals';
+// npm i firebase@9.6.1
+// npm i react-router-dom@5.3.0
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
